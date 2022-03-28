e45acda81ad142371e3c4e56a32efc0e85144fdd
邱光磊 <qiuguanglei@outlook.com>
2022-03-28 15:04:27 +0800

    实现连接数据库功能

---
+++ 连接2014数据库.py
@@ -0,0 +1,14 @@
+# 连接数据库测试，外加查询版本
+
+import pymssql
+
+# db = pymssql.connect(host="localhost", user="sa", password="2013cj1055", database="new", charset="CP936")
+db = pymssql.connect(host="192.168.0.211", user="sa", password="1111", database="AlarmHist", charset="CP936")
+
+cursor = db.cursor()
+# # cursor.execute("SELECT VERSION()")
+# # cursor.execute("select version()")
+# # data = cursor.fetchone()
+if cursor:
+    print("连接成功！")
+db.close()
